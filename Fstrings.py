cebdb63821a64e33251c2736947f650c622da624
aswanisaraj <aswanisaraj@gmail.com>
2023-10-01 12:30:30 +0500

    Third Commit

---
+++ Fstrings.py
@@ -0,0 +1,10 @@
+# F Stings
+# me = "Saraj Aswani"
+# a = "This is %s"%me
+# print(a)
+
+import math
+me = "Lovely"
+a1 =  "6053"
+b = f" this is {me} {a1} {math.tan(66)}"
+print(b)
---
+++ STRING.py
@@ -0,0 +1,7 @@
+string_1 = "hip "
+string_2 = string_1 * 2
+string_3 = "hoo"
+string_4 = "ray!"
+string_5 = string_3 + string_4
+print(string_2, end='')
+print(string_5)
