a8c818bd881aaca041227f7fd97558ba4191b7c5
aswanisaraj <aswanisaraj@gmail.com>
2023-10-08 14:34:25 +0500

    Eighth Commit

---
+++ function+fstring.py
@@ -0,0 +1,13 @@
+
+
+def printhar(string):
+    return f"Ye Lrki sirf Saraj sy shadi krna chahti h {string}"
+
+def add(num1, num2):
+    return num1 + num2 + 3
+
+print("and the name is ",__name__)
+if __name__ == '__main__':
+    print(printhar("Saraj1"))
+    o = add(3, 5)
+    print(o)
