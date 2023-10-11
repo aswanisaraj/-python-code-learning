4d9f55d58cfd07046660e5da6568a44501eecf91
aswanisaraj <aswanisaraj@gmail.com>
2023-10-11 16:52:33 +0500

    Eleventh Commit

---
+++ sets.py
@@ -0,0 +1,5 @@
+# Make sure to print the LIST after removing the duplicates in it
+lst = [0, 12, 3, 4, 12, 4, 12, 34, 12, 33, 7, 8, 2, 90, 23, 45, 12, 33]
+
+new_lst = list(set(lst))
+print(new_lst)
